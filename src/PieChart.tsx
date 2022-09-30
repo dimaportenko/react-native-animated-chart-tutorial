abc5cd25900e88d4e60e76115dcbf05a6a961598
Dima Portenko <troublesohard@gmail.com>
2022-09-30 11:56:53 +0300

    feat: generate chart data and draw it as chart

--- src/PieChart.tsx
+++ src/PieChart.tsx
@@ -1,39 +1,69 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import Svg, {Circle} from 'react-native-svg';
+import {generatePieChartData} from './data';
 
 type PieChartProps = {
   size?: number;
   strokeWidth?: number;
-  strokeColor?: string;
-  percent?: number;
 };
 
-export const PieChart = ({
-  size = 200,
-  strokeWidth = 20,
-  strokeColor = 'black',
-  percent = 0.77,
-}: PieChartProps) => {
+export type PieChartDataItem = {
+  color: string;
+  percent: number;
+};
+
+export type PieChartData = PieChartDataItem[];
+
+export const PieChart = ({size = 200, strokeWidth = 20}: PieChartProps) => {
+  const [data, setData] = React.useState<PieChartData>([]);
+  const [startAngle, setStartAngle] = React.useState<number[]>([]);
   const radius = (size - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
-  const strokeDashoffset = (1 - percent) * circumference;
+  // const strokeDashoffset = (1 - percent) * circumference;
+
+  const refresh = () => {
+    const generatedData = generatePieChartData();
+    let angle = 0;
+    const angles: number[] = [];
+    generatedData.forEach(item => {
+      angles.push(angle);
+      angle += item.percent * 360;
+    });
+    setData(generatedData);
+    setStartAngle(angles);
+  };
+
+  useEffect(() => {
+    refresh();
+  }, []);
 
   return (
     <View>
       <Text style={styles.titleStyle}>PieChart</Text>
       <View style={[{width: size, height: size}, styles.rotate]}>
         <Svg viewBox={`0 0 ${size} ${size}`}>
-          <Circle
-            cx={size / 2}
-            cy={size / 2}
-            r={radius}
-            strokeWidth={strokeWidth}
-            stroke={strokeColor}
-            strokeDashoffset={strokeDashoffset}
-            strokeDasharray={circumference}
-          />
+          {data.map((item, index) => {
+            return (
+              <Circle
+                rotation={startAngle[index]}
+                key={`${item.color}-${index}`}
+                cx={size / 2}
+                cy={size / 2}
+                originX={size / 2}
+                originY={size / 2}
+                r={radius}
+                strokeWidth={strokeWidth}
+                stroke={item.color}
+                strokeDashoffset={(1 - item.percent) * circumference}
+                strokeDasharray={circumference}
+              />
+            );
+          })}
         </Svg>
+      </View>
+      <View style={styles.buttonWrap}>
+        <Button title="Refresh" onPress={refresh} />
       </View>
     </View>
   );
@@ -49,4 +79,5 @@ const styles = StyleSheet.create({
   rotate: {
     transform: [{rotateZ: '-90deg'}],
   },
+  buttonWrap: {marginTop: 20},
 });
---
+++ src/data.ts
@@ -0,0 +1,31 @@
+import {PieChartData} from './PieChart';
+
+export const generatePieChartData = () => {
+  const itemsCount = Math.floor(Math.random() * 7) + 3;
+  const value = [];
+  for (let i = 0; i < itemsCount; i++) {
+    value.push(Math.floor(Math.random() * 60) + 40);
+  }
+
+  const total = value.reduce((a, b) => a + b, 0);
+
+  const data: PieChartData = [];
+  for (let i = 0; i < itemsCount; i++) {
+    const percent = value[i] / total;
+    data.push({
+      percent,
+      color: getRandomColor(),
+    });
+  }
+
+  return data;
+};
+
+const getRandomColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
