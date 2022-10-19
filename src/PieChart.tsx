0b0b1a5488f251da27cfece1ade46525ec981e72
Dima Portenko <troublesohard@gmail.com>
2022-10-19 19:51:43 +0300

    remove final code for template

--- src/PieChart.tsx
+++ src/PieChart.tsx
@@ -1,71 +1,6 @@
-import React, {FC, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
-import Svg, {Circle} from 'react-native-svg';
 import {generatePieChartData} from './data';
-import Animated, {
-  interpolate,
-  useAnimatedProps,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
-
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-
-const ChartArc: FC<{
-  startAngle: number;
-  center: number;
-  radius: number;
-  color: string;
-  strokeWidth: number;
-  percent: number;
-  circumference: number;
-  progress: Animated.SharedValue<number>;
-}> = ({
-  startAngle,
-  center,
-  color,
-  radius,
-  strokeWidth,
-  circumference,
-  percent,
-  progress,
-}) => {
-  const animatedProps = useAnimatedProps(() => {
-    const angle = interpolate(progress.value, [0, 1], [0, startAngle]);
-    const strokeDashoffset = interpolate(
-      progress.value,
-      [0, 1],
-      [circumference, circumference * (1 - percent)],
-    );
-
-    return {
-      strokeDashoffset,
-      transform: [
-        {translateX: center},
-        {translateY: center},
-        {rotate: `${angle}deg`},
-        {translateX: -center},
-        {translateY: -center},
-      ],
-    };
-  }, [startAngle, circumference, percent, progress]);
-
-  return (
-    <AnimatedCircle
-      cx={center}
-      cy={center}
-      originX={center}
-      originY={center}
-      r={radius}
-      strokeWidth={strokeWidth}
-      stroke={color}
-      strokeDasharray={circumference}
-      // @ts-ignore
-      animatedProps={animatedProps}
-    />
-  );
-};
-
 type PieChartProps = {
   size?: number;
   strokeWidth?: number;
@@ -79,28 +14,11 @@ export type PieChartDataItem = {
 export type PieChartData = PieChartDataItem[];
 
 export const PieChart = ({size = 200, strokeWidth = 20}: PieChartProps) => {
-  const progress = useSharedValue(0);
   const [data, setData] = React.useState<PieChartData>([]);
-  const [startAngle, setStartAngle] = React.useState<number[]>([]);
-  const radius = (size - strokeWidth) / 2;
-  const circumference = 2 * Math.PI * radius;
-  const center = size / 2;
-  // const strokeDashoffset = (1 - percent) * circumference;
 
   const refresh = () => {
-    progress.value = 0;
     const generatedData = generatePieChartData();
-    let angle = 0;
-    const angles: number[] = [];
-    generatedData.forEach(item => {
-      angles.push(angle);
-      angle += item.percent * 360;
-    });
     setData(generatedData);
-    setStartAngle(angles);
-    progress.value = withTiming(1, {
-      duration: 1000,
-    });
   };
 
   useEffect(() => {
@@ -110,26 +28,8 @@ export const PieChart = ({size = 200, strokeWidth = 20}: PieChartProps) => {
 
   return (
     <View>
-      <Text style={styles.titleStyle}>PieChart</Text>
-      <View style={[{width: size, height: size}, styles.rotate]}>
-        <Svg viewBox={`0 0 ${size} ${size}`}>
-          {data.map((item, index) => {
-            return (
-              <ChartArc
-                startAngle={startAngle[index]}
-                key={`${item.color}-${index}`}
-                center={center}
-                radius={radius}
-                strokeWidth={strokeWidth}
-                color={item.color}
-                circumference={circumference}
-                percent={item.percent}
-                progress={progress}
-              />
-            );
-          })}
-        </Svg>
-      </View>
+      <Text style={styles.titleStyle}>Pie Chart</Text>
+      <View style={[{width: size, height: size}, styles.rotate]}></View>
       <View style={styles.buttonWrap}>
         <Button title="Refresh" onPress={refresh} />
       </View>
